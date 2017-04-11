5cc9a49377c1bac75dd54e9da2c217169147210a
sakersk <addshakir@gmail.com>
2017-04-11 23:24:42 +0530

    4 commit

---
+++ pattern1.java
@@ -0,0 +1,17 @@
+
+public class pattern1 {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<6;i=i+4)
+		{
+			for(int j=1;j<i+1;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
