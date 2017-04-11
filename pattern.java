6ab80a0c44d23d1fef0bdc95cba52e68039a1c00
sakersk <addshakir@gmail.com>
2017-04-11 23:02:49 +0530

    first commit

---
+++ pattern.java
@@ -0,0 +1,14 @@
+
+public class pattern {
+	public static void main(String[] args) {
+		for(int i=6;i>0;i--)
+		{
+			for(int j=1;j<i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
